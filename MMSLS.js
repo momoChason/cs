3888f0e2047ca0d24193a9506a58a021159fab1f
momoChason <140235821+momoChason@users.noreply.github.com>
2025-05-05 16:10:20 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -3,7 +3,7 @@
  * @param {number} min 最小值（包含）
  * @param {number} max 最大值（不包含）
  */
-const MMSLS = linksEnv ? linksEnv.split(',') : [];
+
 function randomNumber(min = 0, max = 100) {
     return Math.min(Math.floor(min + Math.random() * (max - min)), max);
 }
@@ -11,7 +11,7 @@ function randomNumber(min = 0, max = 100) {
 // 从环境变量中获取链接
 const linksEnv = process.env.MAIMEMO_LINKS;
 
-
+const MMSLS = linksEnv ? linksEnv.split(',') : [];
 const MMSL_random = MMSLS[randomNumber(0, MMSLS.length)];
 
 module.exports = {
