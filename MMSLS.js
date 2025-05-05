99c3d4da8b2641e19d6d953b711ae58cde5adc49
momoChason <140235821+momoChason@users.noreply.github.com>
2025-05-05 17:36:19 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -9,14 +9,14 @@ const linksStr = process.env.MAIMEMO_LINKS;
  * @param {number} min 最小值（包含）
  * @param {number} max 最大值（不包含）
  */
-function randomNumber(min = 0, max = 100) {
-    return Math.min(Math.floor(min + Math.random() * (max - min)), max);
-}
+//function randomNumber(min = 0, max = 100) {
+   // return Math.min(Math.floor(min + Math.random() * (max - min)), max);
+//}
 
-const MMSL_random = MMSLS[randomNumber(0, MMSLS.length)];
+//const MMSL_random = MMSLS[randomNumber(0, MMSLS.length)];
 
 module.exports = {
-    MMSL_random,
-    MMSLS,
+   // MMSL_random,
+    //MMSLS,
     linksStr
 };
