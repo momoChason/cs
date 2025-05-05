c9526c7ce7c0362e957b34f73eb46fc83255a1a0
momoChason <140235821+momoChason@users.noreply.github.com>
2025-05-05 14:48:20 +0800

    Add files via upload

---
+++ MMSLS.js
@@ -0,0 +1,39 @@
+/**
+利用随机概率实现多账户运行
+
+填写格式如下：
+
+
+const MMSLS = [
+    
+    "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
+    "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
+    "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
+  ]
+=====================以上为范例！=====================
+
+=====================请在下面编辑=====================
+=====================请在下面编辑=====================
+=====================请在下面编辑=====================
+=====================请在下面编辑=====================
+=====================请在下面编辑=====================
+ */
+const MMSLS = [
+    
+
+    "https://www.maimemo.com/share/page?uid=16433672&pid=3fb1f6eb08c01b21648f9f75ca2ab92f&tid=43np89fafdfcd8fde80548d4b8e1ab7214",     //yhc
+    
+  ]
+  /**
+   * 生成随机数字
+   * @param {number} min 最小值（包含）
+   * @param {number} max 最大值（不包含）
+   */
+  function randomNumber(min = 0, max = 100) {
+    return Math.min(Math.floor(min + Math.random() * (max - min)), max);
+  }
+  const MMSL_random = MMSLS[randomNumber(0, MMSLS.length)];
+  
+  module.exports = {
+    MMSL_random
+  }
