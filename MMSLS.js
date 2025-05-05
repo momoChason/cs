0a83a12ccec40f69b2239ea30bda4e0aa72177c6
momoChason <140235821+momoChason@users.noreply.github.com>
2025-05-05 17:07:40 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -1,19 +1,19 @@
-const MMSLS = [
-    
+// 从环境变量获取分享链接字符串
+const linksStr = process.env.MAIMEMO_LINKS;
+// 将字符串按逗号分隔成数组
+const MMSLS = linksStr? linksStr.split(',') : [];
 
-    "https://www.maimemo.com/share/page?uid=16433672&pid=3fb1f6eb08c01b21648f9f75ca2ab92f&tid=43np89fafdfcd8fde80548d4b8e1ab7214",     //yhc
-    
-  ]
-  /**
-   * 生成随机数字
-   * @param {number} min 最小值（包含）
-   * @param {number} max 最大值（不包含）
-   */
-  function randomNumber(min = 0, max = 100) {
+/**
+ * 生成随机数字
+ * @param {number} min 最小值（包含）
+ * @param {number} max 最大值（不包含）
+ */
+function randomNumber(min = 0, max = 100) {
     return Math.min(Math.floor(min + Math.random() * (max - min)), max);
-  }
-  const MMSL_random = MMSLS[randomNumber(0, MMSLS.length)];
-  
-  module.exports = {
+}
+
+const MMSL_random = MMSLS[randomNumber(0, MMSLS.length)];
+
+module.exports = {
     MMSL_random
-  }
+};
